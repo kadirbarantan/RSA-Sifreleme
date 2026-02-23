35a8516202ecac71bb50b95d6c92ef593d08f522
kadirbarantan <tankadirbaran0@gmail.com>
2026-02-23 17:39:49 +0300

    algoritma mantığı kodlandı

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace RSA_Example
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.txtD = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrivate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 105);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n:";
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(237, 99);
+            this.txtN.Margin = new System.Windows.Forms.Padding(4);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(148, 30);
+            this.txtN.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 156);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "e:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 209);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "d:";
+            // 
+            // txtE
+            // 
+            this.txtE.Location = new System.Drawing.Point(237, 153);
+            this.txtE.Margin = new System.Windows.Forms.Padding(4);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(148, 30);
+            this.txtE.TabIndex = 4;
+            // 
+            // txtD
+            // 
+            this.txtD.Location = new System.Drawing.Point(237, 203);
+            this.txtD.Margin = new System.Windows.Forms.Padding(4);
+            this.txtD.Name = "txtD";
+            this.txtD.Size = new System.Drawing.Size(148, 30);
+            this.txtD.TabIndex = 5;
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.SystemColors.Info;
+            this.btnFind.Location = new System.Drawing.Point(237, 341);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(139, 75);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Bul";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(102, 256);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Private Key:";
+            // 
+            // txtPrivate
+            // 
+            this.txtPrivate.Location = new System.Drawing.Point(237, 250);
+            this.txtPrivate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrivate.Name = "txtPrivate";
+            this.txtPrivate.Size = new System.Drawing.Size(148, 30);
+            this.txtPrivate.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(578, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "RSA Algoritması – Public Key Kullanarak Private Key Bulma";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(591, 461);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPrivate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtD);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.TextBox txtD;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrivate;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace RSA_Example
+{
+    public partial class Form1 : Form
+    {
+        BigInteger n, ee, d, phi, p, q;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            // Public key al
+            n = BigInteger.Parse(txtN.Text);
+            ee = BigInteger.Parse(txtE.Text);
+
+            // p ve q brute force ile bul
+            for (BigInteger i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    p = i;
+                    q = n / i;
+                    break;
+                }
+            }
+
+            // phi hesapla
+            phi = (p - 1) * (q - 1);
+
+            // d brute force ile bul
+            for (BigInteger i = 1; i < phi; i++)
+            {
+                if ((i * ee) % phi == 1)
+                {
+                    d = i;
+                    break;
+                }
+            }
+
+            // sonucu göster
+            txtD.Text = d.ToString();
+            txtPrivate.Text = "(" + n + ", " + d + ")";
+        }
+    }
+}
